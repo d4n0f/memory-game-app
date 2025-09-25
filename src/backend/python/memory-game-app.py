5a2386bff67c64ff9ccaaa9cfdfc45e495f905aa
Tassi Bence <bence103@mailbox.unideb.hu>
2025-09-25 22:27:37 +0200

    feat: Mysql connection structure

--- src/backend/python/memory-game-app.py
+++ src/backend/python/memory-game-app.py
@@ -14,6 +14,16 @@ db_config = {
     'collation':'latin2_hungarian_ci'
 }
 
+# Mysql szerverrel való kapcsolat felépítése
+def get_db_connect():
+    try:
+        connection = mysql.connector.connect(**db_config)
+        return connection
+    except mysql.connector.Error as err:
+        print("MySQL kapcsolati hiba: {}".format(err))
+        return None
+
+
 if __name__ == '__main__':
     print("Flask inditása")
     app.run(debug=True, host='0.0.0.0', port=5000)
